dcdc465c7f67cba4df390efafc90b6e581b14683
MarkoPolo01 <kovachevich3001@mail.ru>
2022-10-18 22:14:46 +0300

    First

---
+++ app/src/main/java/com/example/students/MainActivity.kt
@@ -0,0 +1,204 @@
+package com.example.students
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.core.os.trace
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var students :MutableList<Student>
+    private lateinit var all_students :MutableList<Student>
+    private lateinit var facultets : MutableList<String?>
+    private lateinit var button_add : Button
+    private lateinit var button_del: Button
+    private lateinit var button_change: Button
+    private lateinit var button_prev : Button
+    private lateinit var button_next : Button
+    private lateinit var button_prev2 : Button
+    private lateinit var button_next2 : Button
+    private lateinit var textView : TextView
+    private lateinit var textView2 : TextView
+
+    var current : Int = 0
+    var current_fac : Int = 0
+    var change : Boolean = false
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        all_students= mutableListOf()
+        students= mutableListOf()
+        facultets= mutableListOf("All")
+        button_add = findViewById(R.id.button_add)
+        button_del = findViewById(R.id.button_del)
+        button_change = findViewById(R.id.button_change)
+        button_next = findViewById(R.id.button_next)
+        button_prev = findViewById(R.id.button_prev)
+        button_next2 = findViewById(R.id.button_next2)
+        button_prev2 = findViewById(R.id.button_prev2)
+        textView = findViewById(R.id.textView)
+        textView2 = findViewById(R.id.textView2)
+        updateTextView2(facultets[current_fac])
+        button_add.setOnClickListener {
+            val saveIntent = Intent(this@MainActivity,
+                SaveActivity::class.java)
+            startActivityForResult(saveIntent, REQUEST_CHOOSE)
+        }
+        button_change.setOnClickListener {
+            val intent = Intent(this@MainActivity, SaveActivity::class.java)
+            intent.putExtra("first_name", students[current].first_name)
+            intent.putExtra("second_name", students[current].second_name)
+            intent.putExtra("last_name", students[current].last_name)
+            intent.putExtra("birth_day", students[current].birth_day)
+            intent.putExtra("faculti", students[current].faculti)
+            intent.putExtra("grup", students[current].group)
+            startActivityForResult(intent, REQUEST_CHOOSE)
+            change = true
+        }
+        button_next.setOnClickListener {
+            if (current < students.size -1){
+                current += 1
+                updateTextView(" "+ students[current].last_name+ "\n "+students[current].first_name+ "\n "+students[current].second_name)
+
+            }
+        }
+        button_prev.setOnClickListener {
+            if (current > 0){
+                current -= 1
+                updateTextView(" "+students[current].last_name+ "\n "+students[current].first_name+ "\n "+students[current].second_name)
+
+            }
+        }
+        button_del.setOnClickListener {
+            if (all_students.size > 0) {
+                all_students.remove(students[current])
+                students.remove(students[current])
+            if (students.size > 0){
+                current = 0
+                updateTextView(" "+students[current].last_name+ "\n "+students[current].first_name+ "\n "+students[current].second_name)
+
+            }else {
+                current = 0
+                if (current_fac > 0){
+                    current_fac = 0
+                    current = 0
+                    updateFaculti()
+                    updateStudents()
+                    updateTextView2(facultets[current_fac])
+                    if (all_students.size > 0) {
+                        updateTextView(" "+students[current].last_name+ "\n "+students[current].first_name+ "\n "+students[current].second_name)
+                    }
+                    else{
+
+                        updateTextView("")
+                    }
+            }
+                else{
+
+                    updateFaculti()
+                    updateTextView("")
+                }
+            }
+                }
+        }
+        button_next2.setOnClickListener {
+            if (current_fac < facultets.size -1){
+                current_fac += 1
+                updateStudents()
+                updateTextView2(facultets[current_fac])
+            }
+        }
+        button_prev2.setOnClickListener {
+            if (current_fac > 0){
+                current_fac -= 1
+                updateStudents()
+                updateTextView2(facultets[current_fac])
+            }
+        }
+    }
+    private fun updateFaculti(){
+        val new_list = mutableListOf<String?>("All")
+        for (item in all_students){
+            new_list.add(item.faculti)
+        }
+        facultets = new_list
+    }
+    private fun updateTextView(text : String?){
+        textView.text = text
+    }
+    private fun updateTextView2(text: String?){
+        textView2.text = text
+    }
+
+//    private fun updateStudents(){
+//        students = if (facultets[current_fac] == "All"){
+//            all_students.toMutableList()
+//        } else{
+//            all_students.filter{ it.faculti == facultets[current_fac] }.toMutableList()
+//        }
+//        current = 0
+//        updateTextView(" "+students[current].last_name+ "\n "+students[current].first_name+ "\n "+students[current].second_name)
+//    }
+    private fun updateStudents(){
+        students = if (facultets[current_fac] == "All"){
+            all_students.toMutableList()
+        } else{
+            all_students.filter{ it.faculti == facultets[current_fac] }.toMutableList()
+        }
+        current = 0
+        if (all_students.size != 0) {
+            updateTextView(" "+students[current].last_name+ "\n "+students[current].first_name+ "\n "+students[current].second_name)
+        } else updateTextView("")
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_CHOOSE) {
+            if (resultCode == AppCompatActivity.RESULT_OK) {
+                if (!change) {
+                    val first_name = data?.getStringExtra("first_name")
+                    val second_name = data?.getStringExtra("second_name")
+                    val last_name = data?.getStringExtra("last_name")
+                    val birth_day = data?.getStringExtra("birth_day")
+                    val faculti = data?.getStringExtra("faculti")
+                    val grup = data?.getStringExtra("grup")
+                    val student = Student(
+                        first_name,
+                        second_name, last_name, birth_day, faculti, grup
+                    )
+                    all_students.add(student)
+                    if (!facultets.contains(student.faculti)) {
+                        facultets.add(student.faculti)
+                    }
+                    updateStudents()
+                    updateTextView(" "+students[current].last_name+ "\n "+students[current].first_name+ "\n "+students[current].second_name)
+                }
+                else{
+                    val first_name = data?.getStringExtra("first_name")
+                    val second_name = data?.getStringExtra("second_name")
+                    val last_name = data?.getStringExtra("last_name")
+                    val birth_day = data?.getStringExtra("birth_day")
+                    val faculti = data?.getStringExtra("faculti")
+                    val grup = data?.getStringExtra("grup")
+                    val student = Student(first_name,
+                        second_name,last_name,birth_day,faculti,grup
+                    )
+                    all_students.remove(students[current])
+                    all_students.add(student)
+                    students[current]=student
+                    if (!facultets[current_fac].equals(student.faculti)&&current_fac!=0){
+                        facultets[current_fac]=student.faculti
+                    }
+                    updateTextView(" "+students[current].last_name+ "\n "+students[current].first_name+ "\n "+students[current].second_name)
+                    updateTextView2(facultets[current_fac])
+                    change = false
+                }
+            }
+            }
+        }
+    companion object{
+        const val REQUEST_CHOOSE = 0
+    }
+}
---
+++ app/src/main/java/com/example/students/SaveActivity.kt
@@ -0,0 +1,187 @@
+package com.example.students
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import java.util.regex.Matcher
+import java.util.regex.Pattern
+
+class SaveActivity : AppCompatActivity() {
+    private lateinit var first_name : EditText
+    private lateinit var button_save : Button
+    private lateinit var button_back :Button
+    private lateinit var second_name : EditText
+    private lateinit var last_name : EditText
+    private lateinit var birth_day : EditText
+    private lateinit var faculti : EditText
+    private lateinit var grup : EditText
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_save)
+        first_name = findViewById(R.id.first_name)
+        second_name = findViewById(R.id.second_name)
+        last_name = findViewById(R.id.last_name)
+        birth_day = findViewById(R.id.birth_day)
+        faculti = findViewById(R.id.faculti)
+        grup = findViewById(R.id.grup)
+        button_save = findViewById(R.id.button_save)
+        button_back = findViewById(R.id.button_back)
+        first_name.setText(intent.getStringExtra("first_name"))
+        last_name.setText(intent.getStringExtra("last_name"))
+        second_name.setText(intent.getStringExtra("second_name"))
+        birth_day.setText(intent.getStringExtra("birth_day"))
+        faculti.setText(intent.getStringExtra("faculti"))
+        grup.setText(intent.getStringExtra("grup"))
+        val answerIntent = Intent()
+        button_save.setOnClickListener{
+            if (checkAllFields()){
+                answerIntent.putExtra("first_name", first_name.text.toString())
+                answerIntent.putExtra("second_name", second_name.text.toString())
+                answerIntent.putExtra("last_name", last_name.text.toString())
+                answerIntent.putExtra("birth_day", birth_day.text.toString())
+                answerIntent.putExtra("faculti", faculti.text.toString())
+                answerIntent.putExtra("grup", grup.text.toString())
+                setResult(RESULT_OK, answerIntent)
+                finish()
+            }
+        }
+        button_back.setOnClickListener {
+            finish()
+        }
+    }
+
+    private fun checkAllFields(): Boolean {
+        if(check(first_name.text.toString())){
+            first_name.error = "Неправильно введены данные"
+            return false
+        }
+        if(check(second_name.text.toString())){
+            second_name.error = "Неправильно введены данные"
+            return false
+        }
+        if(check(last_name.text.toString())){
+            last_name.error = "Неправильно введены данные"
+            return false
+        }
+        if(check3(birth_day.text.toString())){
+            birth_day.error ="Неправидьно введены данные"
+            return false
+        }
+        if(check(faculti.text.toString())){
+            faculti.error = "Неправильно введены данные"
+            return false
+        }
+        if(check2(grup.text.toString())){
+            grup.error = "Неправильно введены данные"
+            return false
+        }
+        return true
+    }
+    private fun check(text:String):Boolean{
+        return text == ""||
+                text.contains(".")||
+                text.contains("/")||
+                text.contains(",")||
+                text.contains(" ")||
+                text.contains("0")||
+                text.contains("1")||
+                text.contains("2")||
+                text.contains("3")||
+                text.contains("4")||
+                text.contains("5")||
+                text.contains("6")||
+                text.contains("7")||
+                text.contains("8")||
+                text.contains("9")||
+                text.contains("(")||
+                text.contains(")")||
+                text.contains("!")||
+                text.contains("@")||
+                text.contains("'")||
+                text.contains('"')||
+                text.contains("{")||
+                text.contains("}")||
+                text.contains("[")||
+                text.contains("]")||
+                text.contains("\\")||
+                text.contains("|")||
+                text.contains("/")||
+                text.contains("?")||
+                text.contains("_")||
+                text.contains("=")||
+                text.contains("+")||
+                text.contains("*")||
+                text.contains("&")||
+                text.contains("^")||
+                text.contains(":")||
+                text.contains("%")||
+                text.contains("$")||
+                text.contains(";")||
+                text.contains("#")||
+                text.contains("№")||
+                text.contains("`")||
+                text.contains("~")
+    }
+    private fun check2(text:String):Boolean{
+        return text == ""||
+                text.contains(".")||
+                text.contains("/")||
+                text.contains(",")||
+                text.contains(" ")||
+                text.contains("(")||
+                text.contains(")")||
+                text.contains("!")||
+                text.contains("@")||
+                text.contains("'")||
+                text.contains('"')||
+                text.contains("{")||
+                text.contains("}")||
+                text.contains("[")||
+                text.contains("]")||
+                text.contains("\\")||
+                text.contains("|")||
+                text.contains("/")||
+                text.contains("?")||
+                text.contains("_")||
+                text.contains("=")||
+                text.contains("+")||
+                text.contains("*")||
+                text.contains("&")||
+                text.contains("^")||
+                text.contains(":")||
+                text.contains("%")||
+                text.contains("$")||
+                text.contains(";")||
+                text.contains("#")||
+                text.contains("№")||
+                text.contains("`")||
+                text.contains("~")
+    }
+//    private fun check3(text:String):Boolean{
+//        if(text.length != 11 ||!text.contains('-')) {
+//            return false
+//        } else {
+//            val year = text.split("-")[0].toInt()
+//            val month = text.split("-")[1].toInt()
+//            val day = text.split("-")[2].toInt()
+//
+//            if(year < 1000) {
+//                return false
+//            }
+//            if(month < 1 || month > 12) {
+//                return false
+//        }
+//            if(day < 1 || day > 31) {
+//                return false
+//        }
+//            return true
+//
+//    }
+private fun check3(text:String):Boolean{
+    val pattern: Pattern = Pattern.compile("(0+[1-9]|[12][0-9]|3[01])-(0+[1-9]|1[012])-((19|20)\\d\\d)")
+    val matcher: Matcher = pattern.matcher(text)
+    return !matcher.find()
+}
+}
